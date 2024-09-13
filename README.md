ce5af086e12b796ff293de22e0724f5f29646260
brucepro <git@brucepro.net>
2024-09-13 13:04:16 -0700

    initial thoughts

---
+++ README.md
@@ -0,0 +1,21 @@
+# Think2Self
+
+## Introduction
+
+*Still in development, but seems to be working. Doesn't play nice with other extensions that use the custom_generate_chat_prompt functions* 
+Think2Self adds extra prompts to each user input to allow the A.I. to develop a plan and have a chain of thought about each input.
+ 
+## Support
+
+If this extension adds value to your AI experience and you'd like to show your appreciation, consider supporting the project:
+
+- [Buy me a coffee](https://www.buymeacoffee.com/brucepro)
+- [Ko-fi](https://ko-fi.com/F1F7U45XV)
+
+## Contributing
+
+Contributions, suggestions, and feedback are always welcome. Please submit issues or pull requests on GitHub, or contact us directly with your ideas and suggestions.
+
+## License
+
+think2self is made available under the MIT License. For more details, see the LICENSE file in the repository.
---
+++ script.py
@@ -0,0 +1,167 @@
+"""
+An example of extension. It does nothing, but you can add transformations
+before the return statements to customize the webui behavior.
+
+Starting from history_modifier and ending in output_modifier, the
+functions are declared in the same order that they are called at
+generation time.
+"""
+
+import gradio as gr
+import torch
+from transformers import LogitsProcessor
+
+from modules import chat, shared
+from modules.text_generation import (
+    decode,
+    encode,
+    generate_reply,
+)
+
+params = {
+    "display_name": "Think2self",
+    "is_tab": False,
+}
+
+class MyLogits(LogitsProcessor):
+    """
+    Manipulates the probabilities for the next token before it gets sampled.
+    Used in the logits_processor_modifier function below.
+    """
+    def __init__(self):
+        pass
+
+    def __call__(self, input_ids, scores):
+        # probs = torch.softmax(scores, dim=-1, dtype=torch.float)
+        # probs[0] /= probs[0].sum()
+        # scores = torch.log(probs / (1 - probs))
+        return scores
+
+def history_modifier(history):
+    """
+    Modifies the chat history.
+    Only used in chat mode.
+    """
+    return history
+
+def state_modifier(state):
+    """
+    Modifies the state variable, which is a dictionary containing the input
+    values in the UI like sliders and checkboxes.
+    """
+    return state
+
+def chat_input_modifier(text, visible_text, state):
+    """
+    Modifies the user input string in chat mode (visible_text).
+    You can also modify the internal representation of the user
+    input (text) to change how it will appear in the prompt.
+    """
+    return text, visible_text
+
+def input_modifier(string, state, is_chat=False):
+    """
+    In default/notebook modes, modifies the whole prompt.
+
+    In chat mode, it is the same as chat_input_modifier but only applied
+    to "text", here called "string", and not to "visible_text".
+    """
+    return string
+
+def bot_prefix_modifier(string, state):
+    """
+    Modifies the prefix for the next bot reply in chat mode.
+    By default, the prefix will be something like "Bot Name:".
+    """
+    return string
+
+def tokenizer_modifier(state, prompt, input_ids, input_embeds):
+    """
+    Modifies the input ids and embeds.
+    Used by the multimodal extension to put image embeddings in the prompt.
+    Only used by loaders that use the transformers library for sampling.
+    """
+    return prompt, input_ids, input_embeds
+
+def logits_processor_modifier(processor_list, input_ids):
+    """
+    Adds logits processors to the list, allowing you to access and modify
+    the next token probabilities.
+    Only used by loaders that use the transformers library for sampling.
+    """
+    processor_list.append(MyLogits())
+    return processor_list
+
+def output_modifier(string, state, is_chat=False):
+    """
+    Modifies the LLM output before it gets presented.
+
+    In chat mode, the modified version goes into history['visible'],
+    and the original version goes into history['internal'].
+    """
+    return string
+
+def custom_generate_chat_prompt(user_input, state, **kwargs):
+    """
+    Replaces the function that generates the prompt from the chat history.
+    Only used in chat mode.
+    """
+    logging = True
+    #Take the user input and develop a plan for how to answer the question.
+    response_text = []
+    question = "Develop a plan to answer the following user input:" + str(user_input)         
+    if logging == True:
+        print(str(question))
+    for response in generate_reply(question, state, is_chat=False, escape_html=False, for_ui=False):
+        response_text.append(response) 
+    if logging == True:
+        print(str(response_text[-1]))
+    theplan = str(question) + str(response_text[-1]) + ". Analyze this plan and provide insights for you to make a final answer."
+    if logging == True:
+        print(str(theplan))
+    #Analyze the plan
+    response_text = []
+    for response in generate_reply(theplan, state, is_chat=False, escape_html=False, for_ui=False):
+        response_text.append(response)    
+    #Create the final answer
+    if logging == True:    
+        print(str(response_text[-1]))
+    final_answer = "Use all of the following data to answer the users question: " + str(user_input) + ". " + str(response_text[-1])
+    if logging == True:
+        print(str(final_answer))
+    response_text = []
+    for response in generate_reply(final_answer, state, is_chat=False, escape_html=False, for_ui=False):
+        response_text.append(response) 
+    if logging == True:
+        print(str(response_text[-1]))
+    result = str(response_text[-1])
+    return result
+
+def custom_css():
+    """
+    Returns a CSS string that gets appended to the CSS for the webui.
+    """
+    return ''
+
+def custom_js():
+    """
+    Returns a javascript string that gets appended to the javascript
+    for the webui.
+    """
+    return ''
+
+def setup():
+    """
+    Gets executed only once, when the extension is imported.
+    """
+    pass
+
+def ui():
+    """
+    Gets executed when the UI is drawn. Custom gradio elements and
+    their corresponding event handlers should be defined here.
+
+    To learn about gradio components, check out the docs:
+    https://gradio.app/docs/
+    """
+    pass
